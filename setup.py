47ddb91ca03967ef00985f865ab7ee0de7aa5719
renjithsraj <renjithsraj@live.com>
2017-03-29 11:17:25 +0530

    added sample application

--- setup.py
+++ setup.py
@@ -20,12 +20,23 @@ mechanism with support for several auth providers.
 setup(
   name = 'payu_biz',
   packages = ['payu_biz'], # this must be the same as the name above
-  version = '1.1',
+  version = '1.1.0',
   description = LONG_DESCRIPTION,
+  license='MIT',
   author = 'Renjith S Raj',
   author_email = 'renjithsraj@live.com',
   url = 'https://github.com/renjithsraj/django_payubiz/tree/master/payu_biz', # use the URL to the github repo
   download_url = 'https://github.com/renjithsraj/django_payubiz/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['payu', 'payment gateway', 'payu gateway django', 'django gateway package'], # arbitrary keywords
-  classifiers = [],
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Internet',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
 )
