8a548dba951b22442dc0bb8968a1e9de271132b7
renjithsraj <renjithsraj@live.com>
2017-03-31 09:40:17 +0530

    added version 1.2.4

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
-LONG_DESCRIPTION = """ payu_biz is wrapper for payu payment gateway for django framework"""
+LONG_DESCRIPTION = """ payu payment gateway package for django"""
 
 
 setup(
   name = 'payu_biz',
   packages = ['payu_biz'], # this must be the same as the name above
-  version = '1.2.1',
+  version = '1.2.4',
   description = LONG_DESCRIPTION,
   license='MIT',
   author = 'Renjith S Raj',
   author_email = 'renjithsraj@live.com',
   url = 'https://github.com/renjithsraj/django_payubiz/tree/master/payu_biz', # use the URL to the github repo
   download_url = 'https://github.com/renjithsraj/django_payubiz/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['payment gateway python package','payu', 'payu python package','payment gateway', 'payu gateway django', 'django gateway package'], # arbitrary keywords
+  keywords = ['payu payment gateway package django','payment gateway python package','payu', 'payu python package','payment gateway', 'payu gateway django', 'django gateway package'], # arbitrary keywords
   classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Topic :: Internet',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
