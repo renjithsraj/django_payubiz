b6f5324fd44a900ba2e87170e10934a1072d01a8
renjithsraj <renjithsraj@live.com>
2018-04-21 02:55:29 +0530

    last commit (deprecated comment)

--- payu_biz/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "Beta Version"
-__author__ = "Renjith S Raj"
-__emailid__ = "renjithsraj@live.com"
--- sample/payudemo_16/payudemo_16/__init__.py
+++
@@ -1,2 +0,0 @@
-print "I have removed the SECRET_KEY from the settings.py please add the parameter"
-# SECRET_KEY = ')pka)ca7@g!63mq3jco5&f9v)(^j0%u3gz@d1h)(t48&2@h%*5'
